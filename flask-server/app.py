d67d15377df8fd934bc51f7b0e70ebf7d831ba6f
LiamB017 <liambedford17@gmail.com>
2024-02-10 12:48:55 +0000

    Retrieving Comment times using datetime

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,71 +1,71 @@
 from flask import Flask, render_template, request, redirect, url_for
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import praw
+import datetime
 
 app = Flask(__name__)
 
 reddit = praw.Reddit(
-    client_id="446egcbq34XRYrgst8DDJg",
-    client_secret="WwOaASS5_DIL8UmwZw12Sis9U3NvOg",
-    user_agent="redditdev sentiment analysis",
-    )
+        client_id="446egcbq34XRYrgst8DDJg",
+        client_secret="WwOaASS5_DIL8UmwZw12Sis9U3NvOg",
+        user_agent="redditdev sentiment analysis",
+        )
 
 @app.route('/sentiment', methods=['POST'])
 def get_sentiment_analysis():
 
+        if request.method == 'POST':
+                topic = request.json.get('topic', '')
+                subreddit_name = request.json.get('subreddit', '')
+                subreddit = reddit.subreddit(subreddit_name) if subreddit_name else None
+                if topic and subreddit:
+                  sinn_fein_posts = subreddit.search(topic, limit=1)
 
-    if request.method == 'POST':
-        topic = request.json.get('topic', '')
-        subreddit_name = request.json.get('subreddit', '')
-        subreddit = reddit.subreddit(subreddit_name) if subreddit_name else None
-        if topic and subreddit:
-          sinn_fein_posts = subreddit.search(topic, limit=1)
+                for post in sinn_fein_posts:
+                        print(post.title)
+                        print("Number of Comments:", len(post.comments))
+                        print(post.url)
+                        print("this is the post")
 
+                        for comment in post.comments.list():
+                                post.comments.replace_more(limit=3)
+                                sfarray = [comment.body for comment in post.comments.list()]
 
-        for post in sinn_fein_posts:
-                print(post.title)
-                print("Number of Comments:", len(post.comments))
-                print(post.url)
-                print("this is the post")
+                        top_comments = []
+                        top_comments_datetime = []
+                        for comment in post.comments[:3]:
+                           if isinstance(comment, praw.models.Comment):
+                            top_comments.append(comment.body)
+                            top_comments_datetime.append(datetime.datetime.fromtimestamp(comment.created_utc))
+                            print("3 TOP COMMENTS: ", comment.body)
 
-        for comment in post.comments.list():
-                post.comments.replace_more(limit=3)
-                sfarray = [comment.body for comment in post.comments.list()]
+                        analyzer = SentimentIntensityAnalyzer()
+                        vscomment = analyzer.polarity_scores(comment.body)
 
-        top_comments = []
-        for comment in post.comments[:3]:
-                top_comments.append(comment.body)
-                print("10 TOP COMMENTS: ", comment.body)
+                        print(sfarray)
 
-        analyzer = SentimentIntensityAnalyzer()
-        vscomment = analyzer.polarity_scores(comment.body)
+                        analyzer = SentimentIntensityAnalyzer()
+                        vs = analyzer.polarity_scores(' '.join(sfarray))
 
-        print(sfarray)
+                        print("top 3 comments", top_comments)
+                        for i, comment_body in enumerate(top_comments[:3], start=1):
+                                print(f"Comment {i}: {comment_body}")
+                                print(f"Comment {i} datetime: {top_comments_datetime[i-1]}")
 
-        analyzer = SentimentIntensityAnalyzer()
-        vs = analyzer.polarity_scores(' '.join(sfarray))
+                        print("Top comment Sentiment ", vscomment['compound'])
+                        print("Sentiment Score ", vs['compound'])
 
+                        if vs['compound'] >= 0.05:
+                                sentiment = " largely Positive"
+                        elif vs['compound'] <= -0.05:
+                                sentiment = " largely Negative"
+                        else:
+                                sentiment = " Neutral"
+                        print("Sentiment is", sentiment)
+                        return {"compound": vs['compound'], "sentiment": sentiment,  "topic": topic, "positive": vs['pos'],"neutral": vs['neu'], "negative":vs['neg'], "subreddit": subreddit_name, "post": post.title, "comments": len(post.comments), "url": post.url, "top_comment": comment.body, "top_comment_sentiment": vscomment,"top3comments": top_comments, "top3commentsdatetime": top_comments_datetime}
 
-        print("top 3 comments", top_comments)
-        for i, comment_body in enumerate(top_comments[:3], start=1):
-                print(f"Comment {i}: {comment_body}")
-
-
-        print("Top comment Sentiment ", vscomment['compound'])
-        print("Sentiment Score ", vs['compound'])
-
-
-        if vs['compound'] >= 0.05:
-                sentiment = " largely Positive"
-        elif vs['compound'] <= -0.05:
-                sentiment = " largely Negative"
-        else:
-                sentiment = " Neutral"
-        print("Sentiment is", sentiment)
-        return {"compound": vs['compound'], "sentiment": sentiment,  "topic": topic, "positive": vs['pos'],"neutral": vs['neu'], "negative":vs['neg'], "subreddit": subreddit_name, "post": post.title, "comments": len(post.comments), "url": post.url, "top_comment": comment.body, "top_comment_sentiment": vscomment,"top3comments": top_comments}
-
-    # Return a response for 'GET' requests or other cases
-    return {"message": "Invalid request"}
+        # Return a response for 'GET' requests or other cases
+        return {"message": "Invalid request"}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+        app.run(debug=True)
--- src/components/TopComments.js
+++ src/components/TopComments.js
@@ -14,14 +14,17 @@ const TopComments = ({ responseData }) => {
           <Typography variant="h4">Top comments</Typography>
           <div>
             <Typography variant="h6">Comment 1</Typography>
+            <Typography>{responseData.top3commentsdatetime[0]}</Typography>
             <Typography>{responseData.top3comments[0]}</Typography>
           </div>
           <div>
             <Typography variant="h6">Comment 2</Typography>
+            <Typography>{responseData.top3commentsdatetime[1]}</Typography>
             <Typography>{responseData.top3comments[1]}</Typography>
           </div>
           <div>
             <Typography variant="h6">Comment 3</Typography>
+            <Typography>{responseData.top3commentsdatetime[2]}</Typography>
             <Typography>{responseData.top3comments[2]}</Typography>
           </div>
         </Grid>
